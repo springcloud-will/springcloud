3a2da288579167da8e5f3767253e08effc76bb4f
Will <willerhe@126.com>
2021-01-01 19:39:42 +0800

    消费服务入驻zookeeper

---
+++ cloud-consumer-zk-order80/src/main/java/com/atme/springcloud/ZkOrderMain.java
@@ -0,0 +1,15 @@
+package com.atme.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZkOrderMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkOrderMain.class, args);
+    }
+
+}
