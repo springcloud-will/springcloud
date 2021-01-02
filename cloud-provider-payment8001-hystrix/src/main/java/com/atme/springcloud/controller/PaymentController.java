50719f1cfd3933c5830fb1a7b29bb7478cff6358
Will <willerhe@126.com>
2021-01-02 12:46:58 +0800

    又添加一个超时接口

---
+++ cloud-provider-payment8001-hystrix/src/main/java/com/atme/springcloud/controller/PaymentController.java
@@ -0,0 +1,74 @@
+package com.atme.springcloud.controller;
+
+import com.atme.springcloud.entities.CommonResult;
+import com.atme.springcloud.entities.Payment;
+import com.atme.springcloud.service.PaymentService;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class PaymentController {
+
+    private final static String InstanceName = "PAYMENT-SERVICE-8001";
+
+    @Resource
+    private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @PostMapping("/payment/create")
+    public CommonResult<Long> create(@RequestBody Payment payment) {
+        Long primaryKey = paymentService.create(payment);
+        if (primaryKey == 0) {
+            return new CommonResult<Long>(InstanceName, "Payment.FailToCreate", "创建支付记录失败");
+        }
+        return new CommonResult<Long>(InstanceName, primaryKey);
+    }
+
+    @GetMapping("/payment/getById")
+    public CommonResult<Payment> getById(Long id) {
+        Payment payment = paymentService.getById(id);
+        if (payment == null) {
+            return new CommonResult<Payment>(InstanceName, "Payment.NotExisted", "支付记录" + id + "不存在");
+        }
+        return new CommonResult<Payment>(InstanceName, payment);
+    }
+
+
+    @GetMapping("/payment/discovery")
+    public CommonResult<Payment> discovery() {
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("services", services);
+        map.put("instances", instances);
+
+        return new CommonResult(InstanceName, map);
+
+    }
+
+    @GetMapping("/payment/discovery/timeout")
+    public CommonResult<String> discoveryTimeout() {
+
+        try {
+            TimeUnit.SECONDS.sleep(6);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return new CommonResult(InstanceName, "你得不到我的");
+
+    }
+}
