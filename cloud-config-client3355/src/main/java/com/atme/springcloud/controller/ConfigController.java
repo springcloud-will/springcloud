55d207e07dbe0615f3d052727cd38a4ed949bd71
Will <willerhe@126.com>
2021-01-03 18:11:56 +0800

    添加配置文件动态化

--- cloud-config-client3355/src/main/java/com/atme/springcloud/controller/ConfigController.java
+++ cloud-config-client3355/src/main/java/com/atme/springcloud/controller/ConfigController.java
@@ -1,10 +1,13 @@
 package com.atme.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+// 不会自动刷新，需要localhost:3355/actuator/refresh 刷新
 public class ConfigController {
 
     @Value("${config.info}")
