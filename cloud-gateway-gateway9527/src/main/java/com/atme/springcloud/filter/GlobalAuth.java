c9e6efd96dd486a15cfbb43e1e42ec6db2ae157e
Will <willerhe@126.com>
2021-01-03 11:17:34 +0800

    添加全局鉴权过滤器

---
+++ cloud-gateway-gateway9527/src/main/java/com/atme/springcloud/filter/GlobalAuth.java
@@ -0,0 +1,30 @@
+package com.atme.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Component
+public class GlobalAuth implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (Objects.requireNonNull(exchange.getRequest().getQueryParams().getFirst("passport")).isEmpty()) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
