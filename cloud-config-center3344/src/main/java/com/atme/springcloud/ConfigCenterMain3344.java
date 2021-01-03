ae73c3e59593b952da441cf4440d433c2744b8f9
Will <willerhe@126.com>
2021-01-03 19:18:44 +0800

    添加选择性更新演示

--- cloud-config-center3344/src/main/java/com/atme/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center3344/src/main/java/com/atme/springcloud/ConfigCenterMain3344.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 // 发送配置中心全量刷新请求 curl -X POST "http://localhost:3344/actuator/bus-refresh"
+// 选择性更新配置 curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355"
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344 {
